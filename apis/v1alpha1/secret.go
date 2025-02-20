89db56ffd16420c30aaba6414ebf0aed016457d9
ci-robot <ci-robot@k8s.io>
2025-02-20 18:32:50 +0000

    Update to ACK runtime `v0.43.0`, code-generator `v0.43.2`

--- apis/v1alpha1/secret.go
+++ apis/v1alpha1/secret.go
@@ -52,6 +52,7 @@ type SecretSpec struct {
 	// you do so, you risk confusion and unexpected results when searching for a
 	// secret by partial ARN. Secrets Manager automatically adds a hyphen and six
 	// random characters after the secret name at the end of the ARN.
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="Value is immutable once set"
 	// +kubebuilder:validation:Required
 	Name *string `json:"name"`
 	// A list of Regions and KMS keys to replicate secrets.
@@ -103,7 +104,7 @@ type SecretStatus struct {
 	// constructed ARN for the resource
 	// +kubebuilder:validation:Optional
 	ACKResourceMetadata *ackv1alpha1.ResourceMetadata `json:"ackResourceMetadata"`
-	// All CRS managed by ACK have a common `Status.Conditions` member that
+	// All CRs managed by ACK have a common `Status.Conditions` member that
 	// contains a collection of `ackv1alpha1.Condition` objects that describe
 	// the various terminal states of the CR and its backend AWS service API
 	// resource
